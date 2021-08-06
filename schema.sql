d84c2a1ab7427ae09d8c1c489416eac65fae2f00
Aino Kuosmanen <aino.kuosmanen@helsinki.fi>
2021-08-06 12:22:35 +0300

    updated schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,34 @@ CREATE TABLE users (
     name TEXT,
     password TEXT
 );
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    topic TEXT,
+    content TEXT,
+    user_id INTEGER REFERENCES users
+);
+
+CREATE TABLE replies (
+    id SERIAL PRIMARY KEY,
+    contet TEXT,
+    message_id INTEGER REFERENCES messages,
+    usre_id INTEGER REFERENCES users
+);
+
+CREATE TABLE qustions (
+    id SERIAL PRIMARY KEY,
+    topic TEXT
+);
+
+CREATE TABLE choices (
+    id SERIAL PRIMARY KEY,
+    question_id INTEGER REFERENCES questions,
+    choice TEXT
+);
+
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    choice_id INTEGER REFERENCES choices,
+    user_id INTEGER REFERENCES users
+);
